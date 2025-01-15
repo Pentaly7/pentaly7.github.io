90a8457748976f2768cb19c29c417678a2b9f86e
lalu.pratama <lalu.pratama@artajasa.co.id>
2025-01-15 12:53:32 +0700

    feat : add planet texture and rotations

--- src/views/FirstPage.vue
+++ src/views/FirstPage.vue
@@ -2,13 +2,14 @@
 import * as THREE from 'three'
 import {onMounted, ref} from "vue";
 import {OrbitControls} from "three/addons";
+import textureAsset from "@/assets/texture.jpg"
 
 /**@type {{value: HTMLCanvasElement}} */
 const mainCanvasRef = ref(null)
 
 onMounted(() => {
   const scene = new THREE.Scene()
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100)
 
   const renderer = new THREE.WebGLRenderer({
     canvas: mainCanvasRef.value
@@ -20,18 +21,31 @@ onMounted(() => {
 
   const planetGeometry = new THREE.SphereGeometry(10, 32, 32)
 
+  const textureLoader = new THREE.TextureLoader();
+  const planetTexture = textureLoader.load(textureAsset); // Update the path to your texture
+  // const debugTexture = new THREE.MeshBasicMaterial(
+  //     {
+  //       color: 0xffffff,
+  //       wireframe: true
+  //     }
+  // )
   const planetMaterial = new THREE.MeshStandardMaterial({
-    color: 0xffffff,
+    map: planetTexture
   })
 
-  const planet = new THREE.Mesh(planetGeometry, planetMaterial)
-  scene.add(planet)
+  // Create a group to hold the planet
+  const planetGroup = new THREE.Group();
+  scene.add(planetGroup);
+
+  const planet = new THREE.Mesh(planetGeometry, planetMaterial);
+  planetGroup.add(planet);
+
 
   const pointLight = new THREE.PointLight(0xffffff, 50)
   pointLight.position.set(-15, 10, 10)
   scene.add(pointLight)
 
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.02)
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.1)
   scene.add(ambientLight)
 
   const lightHelper = new THREE.PointLightHelper(pointLight, 1)
@@ -58,16 +72,21 @@ onMounted(() => {
   ring.rotation.y = Math.PI / 10
   scene.add(ring)
 
+  // planet.rotation.x = -2 / 5 * Math.PI
+  // planet.rotation.z = -(Math.PI / 15)
+
   // allow dragging
   const controls = new OrbitControls(camera, mainCanvasRef.value)
   controls.update()
 
 
+  // Set initial planet rotation (z-axis)
+  planetGroup.rotation.z = -(Math.PI / 15); // Apply z-axis rotation to the group
+
   function animate() {
     requestAnimationFrame(animate)
-    planet.rotation.x += 0.0004
-    planet.rotation.y += 0.001
-    planet.rotation.z += 0.0007
+    planet.rotation.y -= 0.005; // Adjust the speed as needed
+    ring.rotation.z += (Math.PI / 10) * 0.005
     renderer.render(scene, camera)
   }
 
